2ee001f1787dedd39bcff6860777f4e7f5f14b35
pasudo123 <pasudo123@naver.com>
2021-11-18 22:13:46 +0900

    [feat] : alias for @Service

--- src/main/kotlin/com/example/ddd/book/config/CustomAnnotation.kt
+++ src/main/kotlin/com/example/ddd/book/config/CustomAnnotation.kt
@@ -1,5 +1,6 @@
 package com.example.ddd.book.config
 
+import org.springframework.core.annotation.AliasFor
 import org.springframework.stereotype.Service
 
 class CustomAnnotation
@@ -7,9 +8,15 @@ class CustomAnnotation
 @Service
 @Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class DomainService(val value: String = "")
+annotation class DomainService(
+    @get:AliasFor(annotation = Service::class, attribute = "value")
+    val value: String = ""
+)
 
 @Service
 @Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ApplicationService(val value: String = "")
+annotation class ApplicationService(
+    @get:AliasFor(annotation = Service::class, attribute = "value")
+    val value: String = ""
+)
